03a7c0cb001153128ee3398be1c74a9264cf028c
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-02 23:17:14 +0530

    feat: add URL routing for students endpoint in api app

--- django_rest_api/api/urls.py
+++ django_rest_api/api/urls.py
@@ -3,7 +3,5 @@ from . import views
 
 urlpatterns = [
     path('students/', views.studentsView),
-    path('students/<int:pk>/', views.student_detail),
-    path('students/<int:pk>/update/', views.student_update),
-    path('students/<int:pk>/delete/', views.student_delete),    
+   
 ]
