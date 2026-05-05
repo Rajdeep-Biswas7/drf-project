a5899f6350a590fa1bfa7d44cc74e5bc9142b26e
Rajdeep Biswas <coderajdeepbiswas95@gmail.com>
2026-05-05 23:24:47 +0530

    new emplyees create and admin and model changes

--- django_rest_api/emplyees/models.py
+++ django_rest_api/emplyees/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+class Employee(models.Model):
+    emp_name = models.CharField(max_length=50)
+    emp_id = models.CharField(max_length=20)
+    designation = models.CharField(max_length=50)
+    
+    
+    def __str__(self):
+        return self.emp_name    
 # Create your models here.
